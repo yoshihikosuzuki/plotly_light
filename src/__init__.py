3688d8d5153e5de11b9a812d118bf1040af1d91f
yoshihikosuzuki <ys.neoteny@gmail.com>
2021-01-26 16:08:50 +0900

    add scatter options; make a separate renderer file

--- src/__init__.py
+++ src/__init__.py
@@ -7,8 +7,6 @@ from ._show import make_figure, show, show_image
 from ._type import Traces
 from ._config import set_default_layout, set_default_theme, set_default_renderer
 
-# If running in an IPython environment, turn on the connected notebook mode
-
 
 def _notebook_mode() -> None:
     import plotly.offline as py
@@ -16,108 +14,8 @@ def _notebook_mode() -> None:
 
 
 def _set_custom_iframe_renderers() -> None:
-    import os
     import plotly.io as pio
-    from plotly.io._base_renderers import IFrameRenderer
-    from logzero import logger
-    from uuid import uuid4
-    import ipynb_path
-
-    class MyIFrameRenderer(IFrameRenderer):
-        """Custom renderer for `iframe` and `iframe_connected`, in which HTML file
-        names of plots are unique and thus kept forever.
-        """
-
-        def __init__(self,
-                     config=None,
-                     auto_play=False,
-                     post_script=None,
-                     animation_opts=None,
-                     include_plotlyjs=True,
-                     html_directory="iframe_figures"):
-            super().__init__(config,
-                             auto_play,
-                             post_script,
-                             animation_opts,
-                             include_plotlyjs,
-                             html_directory)
-
-            nb_path = ipynb_path.get()
-            self.root_dir, nb_name = os.path.split(nb_path)
-            self.html_directory = f"{nb_name}.iframe_figures"
-            abs_html_directory = f"{nb_path}.iframe_figures"
-            try:
-                os.makedirs(abs_html_directory)
-            except OSError as error:
-                if not os.path.isdir(abs_html_directory):
-                    raise error
-
-        def to_mimebundle(self, fig_dict):
-            from plotly.io import write_html
-
-            # Make iframe size slightly larger than figure size to avoid
-            # having iframe have its own scroll bar.
-            iframe_buffer = 20
-            layout = fig_dict.get("layout", {})
-
-            if layout.get("width", False):
-                iframe_width = str(layout["width"] + iframe_buffer) + "px"
-            else:
-                iframe_width = "100%"
-
-            if layout.get("height", False):
-                iframe_height = layout["height"] + iframe_buffer
-            else:
-                iframe_height = str(525 + iframe_buffer) + "px"
-
-            # Build filename using ipython cell number
-            filename = self.build_filename()
-
-            write_html(
-                fig_dict,
-                filename,
-                config=self.config,
-                auto_play=self.auto_play,
-                include_plotlyjs=self.include_plotlyjs,
-                include_mathjax="cdn",
-                auto_open=False,
-                post_script=self.post_script,
-                animation_opts=self.animation_opts,
-                default_width="100%",
-                default_height=525,
-                validate=False,
-            )
-
-            # Build IFrame
-            iframe_html = """\
-<iframe
-    scrolling="no"
-    width="{width}"
-    height="{height}"
-    src="{src}"
-    frameborder="0"
-    allowfullscreen
-></iframe>
-""".format(
-                width=iframe_width,
-                height=iframe_height,
-                src=self.build_url()
-            )
-
-            return {"text/html": iframe_html}
-
-        def build_filename(self):
-            self.out_fname = f"{self.html_directory}/{uuid4()}.html"
-            logger.debug(f"{self.out_fname}")
-            # NOTE: Always make a plot at the Notebook's directory
-            cwd = os.getcwd()
-            rel = os.path.relpath(os.path.commonprefix([self.root_dir, cwd]), cwd)
-            return f"{rel}/{self.out_fname}"
-
-        def build_url(self):
-            # NOTE: Path for iframe must be a relative path from the root dir
-            return self.out_fname
-
+    from ._renderer import MyIFrameRenderer
     pio.renderers["iframe"] = MyIFrameRenderer(config=pio._renderers.config,
                                                include_plotlyjs=True)
     pio.renderers["iframe_connected"] = MyIFrameRenderer(config=pio._renderers.config,
@@ -131,8 +29,10 @@ except NameError:
 except Exception as e:
     print(e)
 else:
+    # If running in an IPython environment, turn on the connected notebook mode
     _notebook_mode()
     try:
+        # Moreover, if running in a Jupyter notebook, use customized iframe renderer
         _set_custom_iframe_renderers()
     except Exception as e:
         pass
@@ -140,13 +40,9 @@ else:
         set_default_renderer("iframe_connected")
 
 
-# Set Plotly Light's default theme/layout
-
+# Use custom default theme/layout
 set_default_theme("plotly_white")
 set_default_layout(make_layout(font="Arial",
                                font_col="black",
-                               font_size_title=20,
-                               font_size_axis_title=18,
-                               font_size_axis_tick=15,
-                               font_size_legend=15,
-                               margin=dict(l=10, r=10, t=30, b=10)))
+                               font_size=16,
+                               margin=dict(l=10, r=10, t=50, b=10)))
--- src/_scatter.py
+++ src/_scatter.py
@@ -1,5 +1,7 @@
 from typing import Union, Optional, Sequence, Tuple
+from numbers import Number
 import plotly.graph_objects as go
+import numpy as np
 
 
 def make_scatter(x: Sequence,
@@ -8,15 +10,18 @@ def make_scatter(x: Sequence,
                  text_pos: Optional[str] = None,
                  text_size: Optional[float] = None,
                  text_col: Optional[str] = None,
+                 opacity: Optional[float] = None,
                  mode: str = "markers",
                  marker_size: float = 5,
                  marker_width: int = None,
                  line_width: float = 1,
-                 col: Optional[Union[str, Sequence[str]]] = None,
-                 col_range: Optional[Tuple[float, float]] = None,
+                 col: Optional[Union[str, Sequence[Union[int, str]]]] = None,
+                 col_range: Tuple[Optional[float], Optional[float]] = (None, None),
                  col_scale: Optional[str] = None,
                  reverse_scale: bool = False,
-                 show_scale: bool = True,
+                 show_col_bar: bool = False,
+                 col_bar_title: Optional[str] = None,
+                 col_bar_ticks: Optional[Sequence[int]] = None,
                  name: Optional[str] = None,
                  show_legend: bool = False,
                  show_init: bool = True,
@@ -28,32 +33,47 @@ def make_scatter(x: Sequence,
       @ y : Coordinates of data on y-axis.
 
     optional arguments:
-      @ text          : Texts for each data.
-      @ text_pos      : Specify positions of `text`.
-                        Format is: "[top|middle|bottom] [left|center|right]".
-      @ text_size     : Size of `text`.
-      @ text_col      : Color of `text`.
-      @ mode          : "markers", "lines", "markers+lines", "text", etc.
-      @ marker_size   : For "markers" mode.
-      @ marker_width  : For "markers" mode.
-      @ line_width    : For "lines" mode.
-      @ col           : Color of markers and lines.
-      @ col_range     : Value range for color scale.
-      @ col_scale     : Color scale for markers and lines.
-      @ reverse_scale : Reverse `col_scale`.
-      @ show_scale    : Show a scale bar for `col_scale`.
-      @ name          : Display name of the trace in legend.
-      @ show_legend   : Show this trace in legend.
-      @ show_init     : Show this trace initially.
-      @ use_webgl     : Use WebGL instead of SVG for speed.
+      @ text            : Texts for each data.
+      @ text_pos        : Specify positions of `text`.
+                          Format is: "[top|middle|bottom] [left|center|right]".
+      @ text_size       : Size of `text`.
+      @ text_col        : Color of `text`.
+      @ opacity         : Opacity of the trace.
+      @ mode            : "markers", "lines", "markers+lines", "text", etc.
+      @ marker_size     : For "markers" mode.
+      @ marker_width    : For "markers" mode.
+      @ line_width      : For "lines" mode.
+      @ col             : Color of markers and lines.
+      @ col_range       : Value range for color scale.
+      @ col_scale       : Color scale for markers and lines.
+      @ reverse_scale   : Reverse `col_scale`.
+      @ show_col_bar    : Show a color scale bar.
+      @ col_bar_title   : Title text of the color scale bar.
+      @ col_bar_ticks   : Tick values of the color scale bar.
+      @ name            : Display name of the trace in legend.
+      @ show_legend     : Show this trace in legend.
+      @ show_init       : Show this trace initially.
+      @ use_webgl       : Use WebGL instead of SVG for speed.
     """
     assert len(x) == len(y), "`x` and `y` must have same size"
     if text is not None:
         assert len(x) == len(text), "`text` must have same size as data"
+    if col_bar_ticks is not None:
+        assert (isinstance(col, Sequence)
+                and isinstance(col.__iter__().__next__(), Number)), \
+            "`col` must be a list of numbers if `col_bar_ticks` specified"
+        col_bar_ticks = sorted(col_bar_ticks)
+        col_range = (min(min(col),
+                         col_bar_ticks[0],
+                         np.inf if col_range[0] is None else col_range[0]),
+                     max(max(col),
+                         col_bar_ticks[-1],
+                         -np.inf if col_range[0] is None else col_range[1]))
     return (go.Scattergl if use_webgl else go.Scatter)(
         x=x,
         y=y,
         text=text,
+        opacity=opacity,
         mode=mode,
         name=name,
         hoverinfo="text" if text is not None else None,
@@ -63,11 +83,14 @@ def make_scatter(x: Sequence,
         marker=(None if mode == "lines" else
                 dict(size=marker_size,
                      color=col,
-                     cmin=col_range[0] if col_range is not None else None,
-                     cmax=col_range[1] if col_range is not None else None,
+                     colorbar=dict(title=col_bar_title,
+                                   tickmode=None if col_bar_ticks is None else "array",
+                                   tickvals=col_bar_ticks),
                      colorscale=col_scale,
                      reversescale=reverse_scale,
-                     showscale=show_scale if col_scale is not None else None,
+                     showscale=show_col_bar,
+                     cmin=col_range[0],
+                     cmax=col_range[1],
                      line=dict(width=marker_width))),
         line=(None if mode == "markers" else
               dict(width=line_width,
