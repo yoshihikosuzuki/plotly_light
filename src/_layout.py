ace193646174ad670c4aacb09330b11019914a0d
yoshihikosuzuki <ys.neoteny@gmail.com>
2022-08-26 19:23:09 +0900

    add bounding line option to layout; change default col for bounding line and ticks

--- src/_layout.py
+++ src/_layout.py
@@ -10,8 +10,6 @@ def layout(width: Optional[int] = None,
            title: Optional[str] = None,
            x_title: Optional[str] = None,
            y_title: Optional[str] = None,
-           x_standoff: Optional[int] = None,
-           y_standoff: Optional[int] = None,
            x_range: Optional[Tuple[Optional[float], Optional[float]]] = None,
            y_range: Optional[Tuple[Optional[float], Optional[float]]] = None,
            x_logscale: Optional[bool] = None,
@@ -28,12 +26,20 @@ def layout(width: Optional[int] = None,
            y_dtick: Optional[int] = None,
            x_ticklabel: Optional[bool] = None,
            y_ticklabel: Optional[bool] = None,
+           x_standoff: Optional[int] = None,
+           y_standoff: Optional[int] = None,
+           x_bounding_line: Optional[bool] = None,
+           y_bounding_line: Optional[bool] = None,
+           x_mirror: Optional[Union[bool, str]] = None,
+           y_mirror: Optional[Union[bool, str]] = None,
            anchor_axes: Optional[bool] = None,
            grid_col: Optional[str] = None,
            zeroline_col: Optional[str] = None,
+           tick_col: Optional[str] = None,
            tick_len: Optional[float] = None,
            tick_width: Optional[float] = None,
-           tick_col: Optional[str] = None,
+           bounding_line_col: Optional[str] = None,
+           bounding_line_width: Optional[float] = None,
            show_legend: Optional[bool] = None,
            legend_horizontal: Optional[bool] = None,
            legend_coord: Optional[Tuple[Optional[float], Optional[float]]] = None,
@@ -54,17 +60,22 @@ def layout(width: Optional[int] = None,
                              : Font size of title/x,y_title/x,y_tick/legend.
       @ title                : Title of the plot.
       @ [x|y]_title          : Title of [x|y]-axis of the plot.
-      @ [x|y]_standoff       : Size (in px) of the margin between tick labels and axis title.
       @ [x|y]_range          : Range on [x|y]-axis to be drawn in the plot.
       @ [x|y]_grid           : Show grid on [x|y]-axis if True.
       @ [x|y]_zeroline       : Show zeroline on [x|y]-axis if True.
       @ [x|y]_reversed       : Reverse [x|y]-axis if True.
-      @ [x|y]_ticks          : {"" (default), "outside", "inside"}.
+      @ [x|y]_ticks          : {"" (or False; default), "outside" (or True), "inside"}.
       @ [x|y]_dtick          : Distance between two adjacent ticks.
       @ [x|y]_ticklabel      : Show tick labels of [x|y]-axis if True.
+      @ [x|y]_standoff       : Size (in px) of the margin between tick labels and axis title.
+      @ [x|y]_bounding_line  : Show line bewteen axis and plot if True.
+      @ [x|y]_mirror         : {True, "ticks", False, "all", "allticks"}.
+                               Show bounding line/ticks on the other side of the plot.
       @ anchor_axes          : Use same scale for both x/y axes.
       @ [grid|zeroline]_col  : Color of the grids and the zerolines.
-      @ tick_[len|width|col] : Length/width/color of the ticks.
+      @ tick_[col|len|width] : Color/kength/width of the ticks.
+      @ bounding_line_[col|width]
+                             : Color/width of the ticks.
       @ show_legend          : Show legend if True.
       @ legend_horizontal    : Shoe legend horizontally if True.
       @ legend_coord         : Relative coordinate (x, y) of the legend.
@@ -88,6 +99,8 @@ def layout(width: Optional[int] = None,
         xaxis=dict(title=dict(text=x_title, standoff=x_standoff),
                    type="log" if x_logscale is True else None,
                    autorange="reversed" if x_reversed is True else None,
+                   showline=x_bounding_line,
+                   mirror=x_mirror,
                    showgrid=x_grid,
                    zeroline=x_zeroline,
                    showticklabels=x_ticklabel,
@@ -97,12 +110,16 @@ def layout(width: Optional[int] = None,
                    ticklen=tick_len,
                    tickwidth=tick_width,
                    gridcolor=grid_col,
+                   zerolinecolor=zeroline_col,
                    tickcolor=tick_col,
-                   zerolinecolor=zeroline_col),
+                   linecolor=bounding_line_col,
+                   linewidth=bounding_line_width),
         yaxis=dict(title=dict(text=y_title, standoff=y_standoff),
                    type="log" if y_logscale is True else None,
                    autorange="reversed" if y_reversed is True else None,
                    scaleanchor="x" if anchor_axes is True else None,
+                   showline=y_bounding_line,
+                   mirror=y_mirror,
                    showgrid=y_grid,
                    zeroline=y_zeroline,
                    showticklabels=y_ticklabel,
@@ -112,8 +129,10 @@ def layout(width: Optional[int] = None,
                    ticklen=tick_len,
                    tickwidth=tick_width,
                    gridcolor=grid_col,
+                   zerolinecolor=zeroline_col,
                    tickcolor=tick_col,
-                   zerolinecolor=zeroline_col),
+                   linecolor=bounding_line_col,
+                   linewidth=bounding_line_width),
         showlegend=show_legend,
         legend=dict(orientation='h' if legend_horizontal else None,
                     bordercolor=legend_border_col,
