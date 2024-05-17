55d151413b7e30d6ca94b48b208a65dca16a4cfd
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-05-17 13:31:41 +0900

    update image

--- src/_image.py
+++ src/_image.py
@@ -1,53 +1,104 @@
-from typing import Dict, Optional
+from typing import Dict, Optional, Tuple
 
-import plotly.express as px
+import PIL.Image
 import plotly.graph_objects as go
 from IPython.display import Image, display
-from skimage import io
+from logzero import logger
+
+from ._layout import layout
 
 
-def image(fname: str,
-          width: Optional[int] = None,
-          height: Optional[int] = None,
-          margin: Dict = dict(l=0, r=0, t=0, b=0)) -> go.Figure:
-    """Return an interactive figure object of an image loaded from a file.
+def image(
+    in_fname: str,
+    width: Optional[int] = None,
+    height: Optional[int] = None,
+    margin: Dict = dict(l=0, r=0, t=0, b=0),
+    x_range: Optional[Tuple[int, int]] = None,
+    y_range: Optional[Tuple[int, int]] = None,
+    verbose: bool = False,
+):
+    img = PIL.Image.open(in_fname)
+    iw, ih = img.size
+    ar = ih / iw
 
-    positional arguments:
-      @ fname : Name of the image file.
+    if x_range is not None:
+        xb, xe = x_range
+        if y_range is not None:
+            yb, ye = y_range
+        else:
+            yb, ye = 0, (xe - xb) * ar
+    else:
+        if y_range is not None:
+            yb, ye = y_range
+            xb, xe = 0, (ye - yb) / ar
+        else:
+            xb, xe = 0, iw
+            yb, ye = 0, ih
 
-    optional arguments:
-      @ width | height : Of the image.
-    """
-    fig = px.imshow(io.imread(fname))
-    fig.update_layout(width=width,
-                      height=height,
-                      xaxis=dict(showgrid=False,
-                                 zeroline=False,
-                                 showticklabels=False,
-                                 domain=None),
-                      yaxis=dict(showgrid=False,
-                                 zeroline=False,
-                                 showticklabels=False,
-                                 domain=None),
-                      margin=margin)
+    if width is not None:
+        if height is None:
+            height = width * ar
+    else:
+        if height is not None:
+            width = height / ar
+        else:
+            width, height = iw, ih
+
+    if verbose:
+        logger.info(
+            f"plot = W {width:.1f} x H {height:.1f}, range = x[{xb:.1f}..{xe:.1f}] - y[{yb:.1f}..{ye:.1f}]"
+        )
+
+    fig = go.Figure(
+        layout=layout(
+            width=width,
+            height=height,
+            x_range=(xb, xe),
+            y_range=(yb, ye),
+            margin=margin,
+        )
+    )
+    if x_range is None and y_range is None:
+        fig.update_layout(
+            xaxis=dict(showticklabels=False),
+            yaxis=dict(showticklabels=False),
+        )
+    fig.add_layout_image(
+        dict(
+            source=img,
+            xref="x",
+            yref="y",
+            x=xb,
+            y=ye,
+            sizex=xe - xb,
+            sizey=ye - yb,
+            sizing="stretch",
+            layer="below",
+        )
+    )
     return fig
 
 
-def show_image(fname: str,
-               interactive: bool = False,
-               width: Optional[int] = None,
-               height: Optional[int] = None,
-               margin: Dict = dict(l=0, r=0, t=0, b=0)) -> None:
+def show_image(
+    fname: str,
+    static: bool = False,
+    width: Optional[int] = None,
+    height: Optional[int] = None,
+    margin: Dict = dict(l=0, r=0, t=0, b=0),
+    x_range: Optional[Tuple[int, int]] = None,
+    y_range: Optional[Tuple[int, int]] = None,
+    verbose: bool = False,
+) -> None:
     """Quick utility for plotting an image file in Jupyter Notebook.
 
     positional arguments:
       @ fname : Name of the image file.
 
     optional arguments:
-      @ interactive    : If True, plot an interective plot.
+      @ static         : If True, show the image as a non-interactive, static plot.
       @ width | height : Of the image.
     """
-    if not interactive:
+    if static:
         display(Image(fname, width=width, height=height))
     else:
-        image(fname, width, height, margin).show()
+        image(fname, width, height, margin, x_range, y_range, verbose).show()
