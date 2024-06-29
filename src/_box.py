ad1c067e88604a624e7c73a57b9a2da1c9ac5218
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-06-29 22:06:45 +0900

    update

--- src/_box.py
+++ src/_box.py
@@ -1,17 +1,22 @@
-from typing import Union, Optional, List, Tuple
+from typing import Any, List, Optional, Sequence, Tuple, Union
+
 import plotly.graph_objects as go
+
 from . import default
 
 
-def box(data: Union[List, Tuple],
-        name: Optional[str] = None,
-        col: Optional[str] = None,
-        opacity: float = 1,
-        show_points: bool = False,
-        only_points: bool = False,
-        marker_size: Optional[float] = None,
-        show_legend: bool = False,
-        show_init: bool = True) -> go.Box:
+def box(
+    data: Sequence[Any],
+    text: Optional[Sequence[str]] = None,
+    name: Optional[str] = None,
+    col: Optional[str] = None,
+    opacity: float = 1,
+    show_points: bool = False,
+    only_points: bool = False,
+    marker_size: Optional[float] = None,
+    show_legend: bool = False,
+    show_init: bool = True,
+) -> go.Box:
     """Create a simple Trace object of a box plot.
 
     positional arguments:
@@ -26,29 +31,38 @@ def box(data: Union[List, Tuple],
       @ show_legend : Show this trace in legend.
       @ show_init   : Show this trace initially.
     """
-    return go.Box(y=data,
-                  boxpoints='all' if show_points or only_points else None,
-                  pointpos=0 if only_points else None,
-                  hoveron='points' if only_points else None,
-                  fillcolor="rgba(255,255,255,0)" if only_points else None,
-                  line={"color": "rgba(255,255,255,0)"} if only_points else None,
-                  marker={"color": default.layout["colorway"][0]
-                          if col is None and only_points else col,
-                          "size": marker_size},
-                  opacity=opacity,
-                  name=name,
-                  showlegend=show_legend,
-                  visible=None if show_init else "legendonly")
+    return go.Box(
+        y=data,
+        text=text,
+        boxpoints="all" if show_points or only_points else None,
+        pointpos=0 if only_points else None,
+        hoveron="points" if only_points else None,
+        fillcolor="rgba(255,255,255,0)" if only_points else None,
+        line={"color": "rgba(255,255,255,0)"} if only_points else None,
+        marker={
+            "color": (
+                default.layout["colorway"][0] if col is None and only_points else col
+            ),
+            "size": marker_size,
+        },
+        opacity=opacity,
+        name=name,
+        showlegend=show_legend,
+        visible=None if show_init else "legendonly",
+    )
 
 
-def violin(data: Union[List, Tuple],
-           name: Optional[str] = None,
-           col: Optional[str] = None,
-           opacity: float = 1,
-           show_box: bool = False,
-           show_points: bool = False,
-           show_legend: bool = False,
-           show_init: bool = True) -> go.Box:
+def violin(
+    data: Sequence[Any],
+    text: Optional[Sequence[str]] = None,
+    name: Optional[str] = None,
+    col: Optional[str] = None,
+    opacity: float = 1,
+    show_box: bool = False,
+    show_points: bool = False,
+    show_legend: bool = False,
+    show_init: bool = True,
+) -> go.Box:
     """Create a simple Trace object of a box plot.
 
     positional arguments:
@@ -63,11 +77,14 @@ def violin(data: Union[List, Tuple],
       @ show_legend : Show this trace in legend.
       @ show_init   : Show this trace initially.
     """
-    return go.Violin(y=data,
-                     box_visible=show_box,
-                     points="all" if show_points else None,
-                     marker={"color": col},
-                     opacity=opacity,
-                     name=name,
-                     showlegend=show_legend,
-                     visible=None if show_init else "legendonly")
+    return go.Violin(
+        y=data,
+        text=text,
+        box_visible=show_box,
+        points="all" if show_points else None,
+        marker={"color": col},
+        opacity=opacity,
+        name=name,
+        showlegend=show_legend,
+        visible=None if show_init else "legendonly",
+    )
