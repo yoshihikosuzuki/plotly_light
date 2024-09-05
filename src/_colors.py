466b9cc4f0b26772bf7f9c99946322492ab6d297
yoshihikosuzuki <yoshi.suzuki.1992@gmail.com>
2024-09-05 18:41:00 +0900

    update

--- src/_colors.py
+++ src/_colors.py
@@ -1,18 +1,22 @@
 # Human-readable, predefined colors
+
+# blue, lightblue, red, yellow, green, purple are UT colors:
+# https://univtokyo.sharepoint.com/sites/utokyoportal/wiki/SiteAssets/d/utokyo_logotype,utokyo_visual_identity_guidelines/UTokyo_VI_Guidelines.pdf
+
 colors = {
-    "blue": "dodgerblue",
-    "lightblue": "#17becf",
-    "darkblue": "navy",
-    "red": "crimson",
-    "lightred": "salmon",
-    "darkred": "maroon",
-    "yellow": "#ffe11c",
-    "lightyellow": "yellow",
-    "darkyellow": "#ff7f0e",
-    "green": "limegreen",
-    "lightgreen": "lime",
-    "darkgreen": "darkgreen",
-    "purple": "darkviolet",
-    "lightpurple": "violet",
-    "darkpurple": "purple",
+    "blue": "#0b8bee",  # UT blue
+    "lightblue": "#5bdeff",  # UT lightblue
+    "darkblue": "#000080",  # navy
+    "red": "#d71131",  # UT red
+    "lightred": "#fa8072",  # salmon
+    "darkred": "#800000",  # maroon
+    "yellow": "#ffcd00",  # UT yellow
+    "lightyellow": "#ffff00",  # yellow
+    "darkyellow": "#ab7d00",  # variation of mustard yellow
+    "green": "#35ca00",  # UT green
+    "lightgreen": "#00ff00",  # lime
+    "darkgreen": "#006400",  # darkgreen
+    "purple": "#8f07aa",  # UT purple
+    "lightpurple": "#ee82ee",  # violet
+    "darkpurple": "#4e1e66",  # variation of grape
 }
